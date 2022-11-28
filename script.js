44ceb4a132a65a89ff9d6aeafc37b951da839276
Noah <noahmbutcho@gmail.com>
2022-11-28 12:42:48 +0100

    replaced strings with computerSelection and playerSelection arguments

--- script.js
+++ script.js
@@ -25,28 +25,28 @@ function playround(playerSelection, computerSelection) {
 
     if (playerSelection == "rock" && computerSelection == "paper") { // rock vs paper
         computerScore++;
-        return "Paper beats rock, I win";
+        return computerSelection + " beats " + playerSelection + ", I win";
     } else if (playerSelection == "rock" && computerSelection == "scissors") { // rock vs scissors
         userScore++;
-        return "GG's, rock beats scissors... I lost";
+        return "GG's, " + playerSelection + " beats " + computerSelection + "... I lost";
     }              
 
 
     if (playerSelection == "paper" && computerSelection == "rock") { // paper vs rock
         userScore++;
-        return "GG's, paper beats rock... I lost"; 
+        return "GG's, " + playerSelection + " beats " + computerSelection + "... I lost";
     } else if (playerSelection == "paper" && computerSelection == "scissors") { // paper vs scissors
         computerScore++;
-        return "Scissors beats paper, I win !";
+        return computerSelection + " beats " + playerSelection + ", I win";
     }
 
 
     if (playerSelection == "scissors" && computerSelection == "rock") { // scissors vs rock
         computerScore++;
-        return "Rock beats scissors, I win !";
+        return computerSelection + " beats " + playerSelection + ", I win";
     } else if (playerSelection == "scissors" && computerSelection == "paper") { // scissors vs paper
         userScore++;
-        return "GG's, scissors beats paper... I lost";
+        return "GG's, " + playerSelection + " beats " + computerSelection + "... I lost";
     }
 
 
@@ -55,23 +55,17 @@ function playround(playerSelection, computerSelection) {
     }
 }
 
-// const playerSelection = prompt("Please select \"Rock\", \"Paper\" or \"Scissors\"").toLowerCase();
-// const computerSelection = getComputerChoice();
-// console.log(playround(playerSelection, computerSelection));
 
 
 let userScore = parseInt(0);
 let computerScore = parseInt(0);
-let win = "You win"
-let lose = "You lose"
-let tie = "It is a tie"
 
 function game() {
     for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Please select \"Rock\", \"Paper\" or \"Scissors\"").toLowerCase();
+        const playerSelection = prompt("Please select \"Rock\", \"Paper\" or \"Scissors\"").toLowerCase();
         const computerSelection = getComputerChoice();
         console.log(playround(playerSelection, computerSelection));
-        console.log("your score is " + userScore);
+        console.log("Your score is " + userScore);
         console.log("Computer's score is " + computerScore);
     }
 }

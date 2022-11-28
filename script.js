dd03c3096c56d81907cf360f62c0d4e1ab9eb8e3
Noah <noahmbutcho@gmail.com>
2022-11-28 12:27:44 +0100

    add functioning game function and scores

--- script.js
+++ script.js
@@ -24,39 +24,54 @@ function playround(playerSelection, computerSelection) {
     console.log(computerSelection);
 
     if (playerSelection == "rock" && computerSelection == "paper") { // rock vs paper
-        return "Paper beats rock, I win"
+        computerScore++;
+        return "Paper beats rock, I win";
     } else if (playerSelection == "rock" && computerSelection == "scissors") { // rock vs scissors
-        return "GG's, rock beats scissors... I lost"
+        userScore++;
+        return "GG's, rock beats scissors... I lost";
     }              
 
 
     if (playerSelection == "paper" && computerSelection == "rock") { // paper vs rock
-        return "GG's, paper beats rock... I lost" 
+        userScore++;
+        return "GG's, paper beats rock... I lost"; 
     } else if (playerSelection == "paper" && computerSelection == "scissors") { // paper vs scissors
-        return "Scissors beats paper, I win !"
+        computerScore++;
+        return "Scissors beats paper, I win !";
     }
 
 
     if (playerSelection == "scissors" && computerSelection == "rock") { // scissors vs rock
-        return "Rock beats scissors, I win !"
+        computerScore++;
+        return "Rock beats scissors, I win !";
     } else if (playerSelection == "scissors" && computerSelection == "paper") { // scissors vs paper
-        return "GG's, scissors beats paper... I lost"
+        userScore++;
+        return "GG's, scissors beats paper... I lost";
     }
 
 
     else { // rock vs rock / paper vs paper / scissors vs scissors
-        return "Tie game !" 
+        return "Tie game !";
     }
 }
 
-const playerSelection = prompt("Please select \"Rock\", \"Paper\" or \"Scissors\"").toLowerCase();
-const computerSelection = getComputerChoice();
-console.log(playround(playerSelection, computerSelection));
+// const playerSelection = prompt("Please select \"Rock\", \"Paper\" or \"Scissors\"").toLowerCase();
+// const computerSelection = getComputerChoice();
+// console.log(playround(playerSelection, computerSelection));
 
 
+let userScore = parseInt(0);
+let computerScore = parseInt(0);
+let win = "You win"
+let lose = "You lose"
+let tie = "It is a tie"
 
 function game() {
-    for (let i = 1; i < 5; i++) {
-        playround();
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Please select \"Rock\", \"Paper\" or \"Scissors\"").toLowerCase();
+        const computerSelection = getComputerChoice();
+        console.log(playround(playerSelection, computerSelection));
+        console.log("your score is " + userScore);
+        console.log("Computer's score is " + computerScore);
     }
 }

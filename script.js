8c3101374b6afdbd55766c96556b75bfa9fa9958
Noah <noahmbutcho@gmail.com>
2023-02-10 01:50:08 +0100

    refactor rps logic to support DOM events; add DOM events

--- script.js
+++ script.js
@@ -2,74 +2,68 @@
 
 // write a function called getComputerChoice that will randomly return either ‘"Rock", "Paper’"or "Scissors"
 
-function getComputerChoice() {
-    let choices = ["rock", "paper", "scissors"]; // give computer choices
+const choices = ["rock", "paper", "scissors"]; // declare the choices to be usec
+
+const rock = document.getElementById("rock-btn");
+rock.addEventListener("click", () => {
+    playround(choices[0]); 
+});
+
+const paper = document.getElementById("paper-btn");
+paper.addEventListener("click", () => {
+    playround(choices[1]);
+});
+
+const scissors = document.getElementById("scissors-btn");
+scissors.addEventListener("click", () => {
+    playround(choices[2]);
+});
+
+function getComputerSelection() {
     let i = Math.floor(Math.random() * choices.length); // let computer choose randomly
-    
-    if (i === 0) {
-        return "rock";
-    } if (i === 1) {
-        return "paper";
-    } if (i === 2) {
-        return "scissors";
-    }
+    return (choices[i]);
 };
+
+// function game() {
+//     while (computerScore < 5 && userScore < 5) {
+//         const playerSelection = prompt("Please select \"Rock\", \"Paper\" or \"Scissors\"").toLowerCase();
+//         const computerSelection = getComputerChoice();
+//         console.log(playround(playerSelection, computerSelection));
+//         console.log("Player score is " + userScore);
+//         console.log("My score is " + computerScore);
+//     }
+// }
 
 // write a function that plays a single round of rock paper scissors
 // the function should take two parameters: playerSelection and computerSelection
 // then return a string that declares the winner of the round 
 
-function playround(playerSelection, computerSelection) {
+function playround(playerSelection) {
+
+    let computerSelection = getComputerSelection();
+
+    if (
+        playerSelection == choices[0] && computerSelection == choices[1] || // rock vs paper
+        playerSelection == choices[1] && computerSelection == choices[2] || // paper vs scissors
+        playerSelection == choices[2] && computerSelection == choices[0] // scissors vs rock
+    ) { 
+        document.getElementById("scoreboard").innerText = `${computerSelection} beats ${playerSelection}, I win !`;
+        document.getElementById("cs").innerText=Number(document.getElementById("cs").innerText)+1
+    } 
     
-    console.log(computerSelection);
-
-    if (playerSelection == "rock" && computerSelection == "paper") { // rock vs paper
-        computerScore++;
-        return computerSelection + " beats " + playerSelection + ", I win";
-    } else if (playerSelection == "rock" && computerSelection == "scissors") { // rock vs scissors
-        userScore++;
-        return "GG's, " + playerSelection + " beats " + computerSelection + "... I lost";
-    }              
-
-
-    if (playerSelection == "paper" && computerSelection == "rock") { // paper vs rock
-        userScore++;
-        return "GG's, " + playerSelection + " beats " + computerSelection + "... I lost";
-    } else if (playerSelection == "paper" && computerSelection == "scissors") { // paper vs scissors
-        computerScore++;
-        return computerSelection + " beats " + playerSelection + ", I win";
+    else if(computerSelection === playerSelection) {
+        document.getElementById("scoreboard").textContent = `That's crazy, it's a tie game ! We both picked ${playerSelection}`;
     }
 
-
-    if (playerSelection == "scissors" && computerSelection == "rock") { // scissors vs rock
-        computerScore++;
-        return computerSelection + " beats " + playerSelection + ", I win";
-    } else if (playerSelection == "scissors" && computerSelection == "paper") { // scissors vs paper
-        userScore++;
-        return "GG's, " + playerSelection + " beats " + computerSelection + "... I lost";
-    }
-
-
-    else { // rock vs rock / paper vs paper / scissors vs scissors
-        return "It's a tie game ! You both chose " + playerSelection;
+    else {
+        document.getElementById("scoreboard").textContent = `GG's, ${playerSelection} beats ${computerSelection}... I lost`;
+        document.getElementById("ps").innerText=Number(document.getElementById("cs").innerText)+1;
     }
 }
 
 
+// let userScore = parseInt(0);
+// let computerScore = parseInt(0);
 
-let userScore = parseInt(0);
-let computerScore = parseInt(0);
 
-function game() {
-    while (computerScore < 5 && userScore < 5) {
-        const playerSelection = prompt("Please select \"Rock\", \"Paper\" or \"Scissors\"").toLowerCase();
-        const computerSelection = getComputerChoice();
-        console.log(playround(playerSelection, computerSelection));
-        console.log("Player score is " + userScore);
-        console.log("My score is " + computerScore);
-    }
-}
 
-const rock = document.querySelector("#rock-btn")
-const paper = document.querySelector("#paper-btn")
-const scissors = document.querySelector("#scissors-btn")
